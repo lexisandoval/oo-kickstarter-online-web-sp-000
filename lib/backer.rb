8db98e747f3a7b83fe7409b7a810393382816fc2
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-02-02 21:45:58 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -2,8 +2,13 @@ class Backer
 
   attr_reader :backed_projects
 
-  def initialize(backed_projects)
-    @backed_projects = []
+  def initialize(name)
+
+    @backed_projects = name
+
+  end
+
+  def back_project
 
   end
 
